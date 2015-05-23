1db027e04ae81a48b3a0ad5c6eca57d6aa6cd753
Po-Cheng Chu <darkbtf@gmail.com>
2015-05-23 16:51:04 +0800

    send help request

--- src/app/components/edit-table.jsx
+++ src/app/components/edit-table.jsx
@@ -3,35 +3,83 @@ var mui = require('../../../node_modules/material-ui/lib/index');
 
 var FontIcon = mui.FontIcon;
 var TextField = mui.TextField;
-
+var RaisedButton = mui.RaisedButton;
+var DropDownMenu = mui.DropDownMenu;
 var categories = require('../variables.jsx').categories;
-
+var Parse = require('parse').Parse;
+var itemlist=[];
 var EditTable = React.createClass({
-
+  getInitialState: function() {
+    item = [];
+    for(var x in categories) {
+      itemlist.push({
+        text: x,
+        color:categories[x].color,
+        icon:categories[x].icon,
+      });
+    }
+    return {
+      title: "",
+      description: "",
+      time: null,
+      category: "運輸",
+      gender: "female",
+      peopleRequired: null,
+      reward: "",
+      location: new Parse.GeoPoint(30.0,-20.0),
+      locationPlain: "QQ"
+    };
+  },
+  handleChange: function(field) {
+    var self = this;
+    return function(event) {
+      if(field == 'time') self.state[field] = new Date(event.target.value);
+      else if(field == 'peopleRequired') self.state[field] = Number(event.target.value);
+      else self.state[field] = event.target.value;
+      self.forceUpdate();
+      console.log(self.state);
+    };
+  },
+  categoryChange: function(e, index, menuItem){
+    this.state.category = menuItem.text;
+    this.forceUpdate();
+  },
+  sendData: function() {
+    var Case = Parse.Object.extend('Case');
+    var myCase = new Case();
+    for(var key in this.state) myCase.set(key,this.state[key]);
+    myCase.save(null,{
+      success: function(myCase){
+        console.log('save success '+myCase.id);
+      },
+      error:function(myCase,error){
+        console.log('save failed '+error.message);
+      }
+    });
+  },
   render: function() {
-
+    var categoryItem = categories[this.state.category];
     return (
       <div className="edit-table get-help">
         <div className="get-help-title">
           <span className={ "fa fa-user " + this.props.gender } />
-          <TextField hintText="你遇到了什麼困難呢？" />
-          <div className="get-help-category">
-            <span>分類：</span>
-            <span className={ "tag " + "crap" }>
-              <FontIcon className={ "fa fa-" + "motorcycle" } />
-              <span>運輸</span>
-            </span>
-          </div>
+          <TextField hintText="你遇到了什麼困難呢？" onChange={this.handleChange('title')}/>
+        </div>
+        <div className="get-help-category">
+          <span className={"fa fa-"+categoryItem.icon} style={{ backgroundColor: categoryItem.colorCode }}/>
+          <DropDownMenu menuItems={itemlist} onChange={this.categoryChange} />
         </div>
         <div className="get-help-description">
           <span className="fa fa-comment" />
           <TextField
+            onChange={this.handleChange('description')}
             hintText="說得更清楚些吧！"
             multiLine={true} />
         </div>
         <div className="get-help-time">
           <span className="fa fa-clock-o" />
           <TextField
+            onChange={this.handleChange('time')}
             hintText="例如：今天 12:00" />
         </div>
         <div className="get-help-people" >
@@ -39,6 +87,7 @@ var EditTable = React.createClass({
           <span>
             需要
             <TextField
+              onChange={this.handleChange('peopleRequired')}
               hintText="0" />
             名超人
           </span>
@@ -46,11 +95,15 @@ var EditTable = React.createClass({
         <div className="get-help-reward" >
           <span className="fa fa-gift" />
           <TextField
+            onChange={this.handleChange('reward')}
             hintText="例如：一杯星巴克" />
         </div>
         <div className="get-help-location">
           <span className="fa fa-map-marker" />
           <div className="plain-text">(自動定位 OAO)</div>
+        </div>
+        <div className="get-help-button">
+          <RaisedButton label="Send" primary={true} onClick={this.sendData}/>
         </div>
       </div>
     );
@@ -58,6 +111,3 @@ var EditTable = React.createClass({
 });
 
 module.exports = EditTable;
-
-
-
