a30b002c65a30b3606db81209a00bc95f76c0b36
Po-Cheng Chu <darkbtf@gmail.com>
2015-05-23 16:52:28 +0800

    get help table - fetch data from parse api

--- src/app/components/get-help-table.jsx
+++ src/app/components/get-help-table.jsx
@@ -7,9 +7,20 @@ var TextField = mui.TextField;
 var categories = require('../variables.jsx').categories;
 
 var GetHelpTable = React.createClass({
+  getInitialState: function() {
+    return {
+      title: "",
+      time: "",
+      category: "",
+      peopleRequired: null,
+      reward: "",
+      locationPlain: ""
+    };
+  },
 
   render: function() {
-
+    //console.log(this.props);
+    console.log(this.state);
     return (
       <div className="get-help plain-text-block">
         <div className="get-help-title">
@@ -18,29 +29,29 @@ var GetHelpTable = React.createClass({
             <span>分類：</span>
             <span className={ "tag " + "crap" }>
               <FontIcon className={ "fa fa-" + "motorcycle" } />
-              <span>運輸</span>
+              <span>{ this.state.category }</span>
             </span>
           </div>
         </div>
         <div className="get-help-description">
           <span className="fa fa-comment" />
-          <div className="plain-text">今天很想哭很想哭很想哭很想哭很想哭很想哭很想哭很想哭很想哭很想哭很想哭很想哭很想哭很想哭很想哭很想哭很想哭很想哭很想哭</div>
+          <div className="plain-text">{this.state.title}</div>
         </div>
         <div className="get-help-time">
           <span className="fa fa-clock-o" />
-          <div className="plain-text">今天 12:00</div>
+          <div className="plain-text">{ this.state.time.toString() }</div>
         </div>
         <div className="get-help-people" >
           <span className="fa fa-group" />
-          <div className="plain-text">需要 1 名超人</div>
+          <div className="plain-text">需要 { this.state.peopleRequired } 名超人</div>
         </div>
         <div className="get-help-reward" >
           <span className="fa fa-gift" />
-          <div className="plain-text">一杯星巴克</div>
+          <div className="plain-text">{ this.state.reward }</div>
         </div>
         <div className="get-help-location">
           <span className="fa fa-map-marker" />
-          <div className="plain-text">(自動定位 OAO)</div>
+          <div className="plain-text">{ this.state.locationPlain }</div>
         </div>
       </div>
     );
