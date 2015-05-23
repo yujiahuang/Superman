eb2987bf0d4686c7fe31a3e45656f80822efc252
Po-Cheng Chu <darkbtf@gmail.com>
2015-05-23 16:51:16 +0800

    Add color code for every category

--- src/app/variables.jsx
+++ src/app/variables.jsx
@@ -1,36 +1,47 @@
 var categories = {
   '運輸': {
     icon: 'motorcycle',
-    color: 'crap'
+    color: 'crap',
+    colorCode: '#A67522'
   },
   '教學': {
     icon: 'book',
-    color: 'lavender'
+    color: 'lavender',
+    colorCode: '#AD49AD'
   },
   '科技': {
     icon: 'desktop',
-    color: 'sky'
+    color: 'sky',
+    colorCode: '#1A92AD'
   },
   '餐點': {
     icon: 'coffee',
-    color: 'crimson'
+    color: 'crimson',
+    colorCode: '#DB3333'
   },
   '修理': {
     icon: 'wrench',
-    color: 'grass'
+    color: 'grass',
+    colorCode: '#89AD1A'
   },
   '其他': {
     icon: 'rocket',
-    color: 'iron'
+    color: 'iron',
+    colorCode: '#8d8d8d'
   }
 };
 
-var right_buttons = {
+var leftButtons = {
+  'back': 'angle-left'
+};
+
+var rightButtons = {
   'edit': 'pencil-square-o',
   'location': 'location-arrow'
-}
+};
 
 module.exports = {
   categories: categories,
-  right_buttons: right_buttons
+  rightButtons: rightButtons,
+  leftButtons: leftButtons
 };
